8b3f4f03f306904d17c8ee7b7d6490880fb7ddc9
Adrian Palma <adrianmilwid@gmail.com>
2026-03-13 18:52:20 +0100

    Changed value so now it list correctly. Closes #27

--- resources/views/actors/list.blade.php
+++ resources/views/actors/list.blade.php
@@ -1,12 +1,5 @@
-@extends('layouts.master')
-
-@section('title', 'Listado de actores')
-@section('header_title', $title)
-@section('header_subtitle', 'Listado con información básica de actores')
-
-@section('content')
-
-@if($actors->isEmpty())
+@extends('layouts.master') @section('title', 'Listado de actores') @section('header_title', $title) @section('header_subtitle',
+'Listado con información básica de actores') @section('content') @if ($actors->isEmpty())
     <div class="alert alert-warning text-center mb-0" role="alert">
         <h5 class="mb-2">No se ha encontrado ningún actor</h5>
         <p class="mb-0 text-muted">Prueba otra sección o añade un nuevo actor.</p>
@@ -22,23 +15,16 @@
                     <th>Salario</th>
                 </tr>
             </thead>
-
             <tbody>
-                @foreach($actors as $actor)
+                @foreach ($actors as $actor)
                     <tr class="text-center align-middle">
                         <td class="text-left font-weight-bold">{{ $actor->name }}</td>
                         <td>{{ $actor->birth_date }}</td>
                         <td>{{ $actor->country }}</td>
-                        <td>
-                            <span class="badge badge-info">
-                                {{ $actor->salaries }}
-                            </span>
-                        </td>
+                        <td> <span class="badge badge-info"> {{ $actor->salary }} </span> </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
-@endif
-
-@endsection
+@endif @endsection
